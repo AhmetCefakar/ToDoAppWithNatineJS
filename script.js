1ece792f835c49ef4926965a5ff807ab01dcc33d
ahmetcefakar <ahmetcefakar@yandex.com>
2018-04-14 21:36:15 +0300

    Added close click event every list items.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+
 var itemList = ['item1', 'item2', 'item3', 'item4', 'item5']; // Initialize list.
 
 var myListTag = document.querySelector('#mylist');
 
-itemList.forEach(function(item){
+// Adding every items to UI list.
+itemList.forEach(function (item) {
     // '<li class="list-group-item">' + item + '<span class="close">x</span></li>'
     var li = document.createElement('li');
     var text = document.createTextNode(item);
@@ -18,9 +20,25 @@ itemList.forEach(function(item){
 
     li.appendChild(span);
 
-    // Add an event listener for every li tag.
-    li.addEventListener('click',function(){
+    // Adding an event listener for every li tag.
+    li.addEventListener('click', function () {
         this.classList.toggle('checked');
     });
+    
+    //  Adding an event listener for every 'X' close button.
+    span.onclick = function(){
+        var liParent = this.parentElement;
+        liParent.style.display = 'none';
+    };
 });
 
+
+//  Adding an event listener for every 'X' close button.
+//var closeButtons = document.querySelectorAll('.close');
+
+//closeButtons.forEach(function(itemClose){
+//    itemClose.onclick = function(){
+//        var liParent = this.parentElement;
+//        liParent.style.display = 'none';
+//    };
+//});
